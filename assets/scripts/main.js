7cdae3ee6485397e5900b3b4861b8bcea27276e9
gtz05050708 <q6zhang@ucsd.edu>
2021-02-03 04:26:47 -0800

    fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
 // main.js
 // number and slider as well as volume icon
+var hornSound = document.getElementById("horn-sound");
+var hornButton = document.getElementById("honk-btn");
 var slider = document.getElementById("volume-slider");
 var number = document.getElementById("volume-number");
 var icon = document.getElementById("volume-image");
@@ -7,20 +9,25 @@ slider.addEventListener("input", changeNumber);
 number.addEventListener("input", changeSlider);
 function changeIcon() {
     if (number.value == 0) {
-        icon.src="./assets/media/icons/volume-level-0.svg"
+        hornButton.disabled = true;
+        icon.src="./assets/media/icons/volume-level-0.svg";
     }
     else if (number.value <= 33) {
-        icon.src="./assets/media/icons/volume-level-1.svg"
+        hornButton.disabled = false;
+        icon.src="./assets/media/icons/volume-level-1.svg";
     }
     else if (number.value <= 66) {
-        icon.src="./assets/media/icons/volume-level-2.svg"
+        hornButton.disabled = false;
+        icon.src="./assets/media/icons/volume-level-2.svg";
     }
     else {
-        icon.src="./assets/media/icons/volume-level-3.svg"
+        hornButton.disabled = false;
+        icon.src="./assets/media/icons/volume-level-3.svg";
     }
 }
 function changeNumber() {
     number.value = slider.value;
+    hornSound.volume = slider.value/100;
     changeIcon();
 }
 function changeSlider() {
@@ -32,6 +39,7 @@ function changeSlider() {
         number.value = 100
     }
     slider.value = number.value;
+    hornSound.volume = number.value/100;
     changeIcon();
 }
 
@@ -39,14 +47,15 @@ function changeSlider() {
 var air = document.getElementById("radio-air-horn");
 var car = document.getElementById("radio-car-horn");
 var party = document.getElementById("radio-party-horn");
-air.addEventListener("input", changeImgAudio(air.id));
-car.addEventListener("input", changeImgAudio(car.id));
-party.addEventListener("input", changeImgAudio(party.id));
+
+air.addEventListener("change", function() {changeImgAudio(air.id);});
+car.addEventListener("change", function() {changeImgAudio(car.id);});
+party.addEventListener("change", function() {changeImgAudio(party.id);});
 
 function changeImgAudio(radio) {
     let soundImage = document.getElementById("sound-image");
     let hornSound = document.getElementById("horn-sound");
-    switch (radio.id) {
+    switch (radio) {
         case 'radio-party-horn' :
             soundImage.src = "./assets/media/images/party-horn.svg";
             hornSound.src = "./assets/media/audio/party-horn.mp3";
@@ -63,11 +72,8 @@ function changeImgAudio(radio) {
 }
 
 // sound the horn when button is pressed
-var hornButton = document.getElementById("honk-btn");
-document.addEventListener("submit", Honhon);
+hornButton.addEventListener("submit", Honhon);
 function Honhon() {
-   let hornSound = document.getElementById("horn-sound");
-   hornSound.volume = number.value/100;
-   hornSound.play();
-}
-
+    hornSound.volume = number.value/100;
+    hornSound.play();
+ }
