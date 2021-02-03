31d8992a407884fe51555023ef7518164f6dab7d
gtz05050708 <q6zhang@ucsd.edu>
2021-02-03 03:20:16 -0800

    Initial commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,73 @@
 // main.js
+// number and slider as well as volume icon
+var slider = document.getElementById("volume-slider");
+var number = document.getElementById("volume-number");
+var icon = document.getElementById("volume-image");
+slider.addEventListener("input", changeNumber);
+number.addEventListener("input", changeSlider);
+function changeIcon() {
+    if (number.value == 0) {
+        icon.src="./assets/media/icons/volume-level-0.svg"
+    }
+    else if (number.value <= 33) {
+        icon.src="./assets/media/icons/volume-level-1.svg"
+    }
+    else if (number.value <= 66) {
+        icon.src="./assets/media/icons/volume-level-2.svg"
+    }
+    else {
+        icon.src="./assets/media/icons/volume-level-3.svg"
+    }
+}
+function changeNumber() {
+    number.value = slider.value;
+    changeIcon();
+}
+function changeSlider() {
+    // limit value to be between 0 - 100
+    if (number.value < 0) {
+        number.value = 0
+    } 
+    else if (number.value > 100) {
+        number.value = 100
+    }
+    slider.value = number.value;
+    changeIcon();
+}
 
-// TODO
+// change horn image and audio
+var air = document.getElementById("radio-air-horn");
+var car = document.getElementById("radio-car-horn");
+var party = document.getElementById("radio-party-horn");
+air.addEventListener("input", changeImgAudio(air.id));
+car.addEventListener("input", changeImgAudio(car.id));
+party.addEventListener("input", changeImgAudio(party.id));
+
+function changeImgAudio(radio) {
+    let soundImage = document.getElementById("sound-image");
+    let hornSound = document.getElementById("horn-sound");
+    switch (radio.id) {
+        case 'radio-party-horn' :
+            soundImage.src = "./assets/media/images/party-horn.svg";
+            hornSound.src = "./assets/media/audio/party-horn.mp3";
+            break;
+        case 'radio-air-horn' :
+            soundImage.src = "./assets/media/images/air-horn.svg";
+            hornSound.src = "./assets/media/audio/air-horn.mp3";
+            break;
+        case 'radio-car-horn' :
+            soundImage.src = "./assets/media/images/car.svg";
+            hornSound.src = "./assets/media/audio/car-horn.mp3";
+            break;
+    }
+}
+
+// sound the horn when button is pressed
+var hornButton = document.getElementById("honk-btn");
+document.addEventListener("submit", Honhon);
+function Honhon() {
+   let hornSound = document.getElementById("horn-sound");
+   hornSound.volume = number.value/100;
+   hornSound.play();
+}
+
